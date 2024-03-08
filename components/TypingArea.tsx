57d7cdd39ab87c8638cde8e2724a6991b01b7c1b
sangeon1500 <105337019+sangeon1500@users.noreply.github.com>
2024-03-08 15:34:40 +0900

    react-keyboard 설치 및 한글 적용

--- components/TypingArea.tsx
+++ components/TypingArea.tsx
@@ -1,23 +1,70 @@
-import dynamic from "next/dynamic"
-import Typing, { TypingMultiline } from "react-kr-typing-anim"
+// import Typing, { TypingMultiline } from "react-kr-typing-anim"
+import * as Hangul from "hangul-js"
+import styles from "../styles/components/TypingArea.module.scss"
+import { useState } from "react"
+import KeyboardReact from "react-simple-keyboard"
 
 const TypingArea = () => {
-  return (
-    <div>
-      <Typing
-        Tag="div"
-        preDelay={1000}
-        postDelay={1000}
-        cursor
-        fixedWidth
-        onDone={() => console.log("done")}
-      >
-        환영합니다!
-      </Typing>
+  const [layoutName, setLayoutName] = useState("default")
 
-      <TypingMultiline
-        strs={`환영합니다!
-          즐거운 하루 되세요!`}
+  const [text, setText] = useState<string>("아이엠랩")
+
+  const koreanLayout = {
+    default: [
+      "ㅂ ㅈ ㄷ ㄱ ㅅ ㅛ ㅕ ㅑ ㅐ ㅔ",
+      "ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ",
+      "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ {pre}",
+      "{space} {dot} {enterText}",
+    ],
+    shift: [
+      "ㅃ ㅉ ㄸ ㄲ ㅆ ㅛ ㅕ ㅑ ㅒ ㅖ",
+      "ㅁ ㄴ ㅇ ㄹ ㅎ ㅗ ㅓ ㅏ ㅣ",
+      "{shift} ㅋ ㅌ ㅊ ㅍ ㅠ ㅜ ㅡ {pre}",
+      "{space} {dot} {enterText}",
+    ],
+  }
+
+  const onKeyPress = (key: string) => {
+    if (key === "{pre}") {
+      const res = text.slice(0, -1)
+      setText(res)
+    } else if (key === "{shift}") {
+      setLayoutName((prev) => (prev === "default" ? "shift" : "default"))
+    } else if (key === "{enterNum}" || key === "{enterText}") {
+      console.log("enter clicked!")
+    } else if (key === "{dot}") {
+      setText((prev) => prev + ".")
+    } else if (key === "{space}") {
+      setText((prev) => prev + " ")
+    } else {
+      setText((prev) => Hangul.assemble(Hangul.disassemble(prev + key)))
+    }
+  }
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.box}>
+        <input
+          className={styles.input}
+          onChange={(e) => {
+            setText(e.target.value)
+          }}
+        />
+        <p className={styles.exampleText}>{text}</p>
+      </div>
+      <KeyboardReact
+        layoutName={layoutName}
+        layout={{ ...koreanLayout }}
+        onKeyPress={onKeyPress}
+        display={{
+          "{enterText}": "Enter",
+          "{shift}": "↑",
+          "{.}": ".",
+          "{space}": " ",
+          "{dot}": ".",
+          "{pre}": "←",
+        }}
+        text
       />
     </div>
   )
